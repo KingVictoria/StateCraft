1620dea0a6871613d2cf352f0c0c648ac7e38db9
KingVictoria <TheRealKingVictoria@gmail.com>
2018-01-03 18:03:22 -0500

    First Commit

---
+++ StateCraft/src/StateCraft.java
@@ -0,0 +1,13 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StateCraft extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+	}
+	
+	@Override
+	public void onDisable(){
+	}
+
+}
